e0e71be85b668cb66cfccbafde6d1a5674e70aad
Jen Janik <jenmjanik@gmail.com>
2021-05-16 22:24:12 -0500

    simplified the User model

--- src/main/java/com/marketplace/demo/model/User.java
+++ src/main/java/com/marketplace/demo/model/User.java
@@ -24,43 +24,50 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Column
-    private String firstName;
+//    @Column
+//    private String firstName;
+//
+//    @Column
+//    private String lastName;
+//
+//    @Column
+//    private String street1;
+//
+//    @Column
+//    private String street2;
+//
+//    @Column
+//    private String city;
+//
+//    @Column
+//    private String state;
+//
+//    @Column
+//    private String zip;
 
-    @Column
-    private String lastName;
 
-    @Column
-    private String street1;
+//    public User(Long userID, String userName, String email, String password, String firstName, String lastName, String street1, String street2, String city, String state, String zip) {
+//        this.userID = userID;
+//        this.userName = userName;
+//        this.email = email;
+//        this.password = password;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.street1 = street1;
+//        this.street2 = street2;
+//        this.city = city;
+//        this.state = state;
+//        this.zip = zip;
+//    }
 
-    @Column
-    private String street2;
+    public User() {
+    }
 
-    @Column
-    private String city;
-
-    @Column
-    private String state;
-
-    @Column
-    private String zip;
-
-
-    public User(Long userID, String userName, String email, String password, String firstName, String lastName, String street1, String street2, String city, String state, String zip) {
+    public User(Long userID, String userName, String email, String password) {
         this.userID = userID;
         this.userName = userName;
         this.email = email;
         this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.street1 = street1;
-        this.street2 = street2;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-    }
-
-    public User() {
     }
 
     public Long getUserID() {
@@ -95,62 +102,6 @@ public class User {
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getStreet1() {
-        return street1;
-    }
-
-    public void setStreet1(String street1) {
-        this.street1 = street1;
-    }
-
-    public String getStreet2() {
-        return street2;
-    }
-
-    public void setStreet2(String street2) {
-        this.street2 = street2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
     @Override
     public String toString() {
         return "User{" +
@@ -158,13 +109,79 @@ public class User {
                 ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", street1='" + street1 + '\'' +
-                ", street2='" + street2 + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip='" + zip + '\'' +
                 '}';
     }
+
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getStreet1() {
+//        return street1;
+//    }
+//
+//    public void setStreet1(String street1) {
+//        this.street1 = street1;
+//    }
+//
+//    public String getStreet2() {
+//        return street2;
+//    }
+//
+//    public void setStreet2(String street2) {
+//        this.street2 = street2;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//
+//    public void setState(String state) {
+//        this.state = state;
+//    }
+//
+//    public String getZip() {
+//        return zip;
+//    }
+//
+//    public void setZip(String zip) {
+//        this.zip = zip;
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "userID=" + userID +
+//                ", userName='" + userName + '\'' +
+//                ", email='" + email + '\'' +
+//                ", password='" + password + '\'' +
+//                ", firstName='" + firstName + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                ", street1='" + street1 + '\'' +
+//                ", street2='" + street2 + '\'' +
+//                ", city='" + city + '\'' +
+//                ", state='" + state + '\'' +
+//                ", zip='" + zip + '\'' +
+//                '}';
+//    }
 }
