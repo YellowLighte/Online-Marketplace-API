75b6e39351c955cdf30e9a4f90052febd9fad19e
Jen Janik <jenmjanik@gmail.com>
2021-05-17 09:35:49 -0500

    cleaned up ProductController and ProductService

--- src/main/java/com/marketplace/demo/controller/ProductController.java
+++ src/main/java/com/marketplace/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.marketplace.demo.controller;
 import com.marketplace.demo.exception.InformationNotFoundException;
 import com.marketplace.demo.model.Product;
 import com.marketplace.demo.repository.ProductRepository;
+import com.marketplace.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +14,35 @@ import java.util.Optional;
 @RequestMapping(path = "/api")
 public class ProductController {
 
-    private ProductRepository productRepository;
+//    private ProductRepository productRepository;
+//
+//    @Autowired
+//    public void setProductRepository(ProductRepository productRepository) {
+//        this.productRepository = productRepository;
+//    }
+    private ProductService productService;
 
     @Autowired
-    public void setProductRepository(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
     }
+
 
     @GetMapping("/products")
     public List<Product> getProducts() {
-        return productRepository.findAll();
+        return productService.getProducts();
     }
 
     @GetMapping("/products/{productId}")
     public Optional<Product> getProduct(@PathVariable Long productId) {
-        Optional product = productRepository.findById(productId);
-        if (product.isPresent()) {
-            return product;
-        } else {
-            throw new InformationNotFoundException("product with id " + productId + " not found.");
-        }
+        return productService.getProduct(productId);
     }
 
     //localhost:9092/api/products
     @PostMapping("/products")
     public Product createProduct(@RequestBody Product productObject) {
-        return productRepository.save(productObject);
+        return productService.createProduct(productObject);
     }
+
+
 }
--- src/main/java/com/marketplace/demo/service/ProductService.java
+++ src/main/java/com/marketplace/demo/service/ProductService.java
@@ -1,4 +1,38 @@
 package com.marketplace.demo.service;
 
+import com.marketplace.demo.exception.InformationNotFoundException;
+import com.marketplace.demo.model.Product;
+import com.marketplace.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProduct(Long productId) {
+        Optional product = productRepository.findById(productId);
+        if (product.isPresent()) {
+            return product;
+        } else {
+            throw new InformationNotFoundException("product with id " + productId + " not found.");
+        }
+    }
+
+    public Product createProduct(Product productObject) {
+        return productRepository.save(productObject);
+    }
 }
